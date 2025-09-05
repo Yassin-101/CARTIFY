12361c015a849284102fe86b43a2c612e7d2be43
Yassin <mirza.yassin9@gmail.com>
2025-09-05 14:15:49 +0400

    add changes

--- frontend/src/Component/AdminSidebar.jsx
+++ frontend/src/Component/AdminSidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AdminSidebar = () => {
   return (
     <div className='w-64 bg-white border-r p-6 flex flex-col'>
-      
+      <h2 className='text-2xl font-bold text-red-600 mb-8'>CARTIFY ADMIN</h2>
     </div>
   )
 }
