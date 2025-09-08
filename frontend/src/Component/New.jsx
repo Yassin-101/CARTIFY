a53e6d7330373b94137d78bd6c5d9c78d99efad3
Yassin <mirza.yassin9@gmail.com>
2025-09-09 02:26:46 +0400

    new-collection component

--- frontend/src/Component/New.jsx
+++ frontend/src/Component/New.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const New = () => {
+const New = ({category}) => {
+  const [latest,setLatest] = useState({
+    women:[],
+    men:[],
+    kid:[]
+  })
+  useEffect(()=>{
+    const fetchNew = async()=>{
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+  },[])
   return (
     <div>
-      
+      <div className='text-left py-8 px-5 text-xl flex flex-row justify-between '>
+        <p className='text-2xl'>NEW IN</p>
+        <p className='underline cursor-pointer uppercase'>View All</p>
+      </div>
+      {/* All 12 images */}
+      <div className='grid grid-cols-2 md:grid-cols-6'>
+
+      </div>
     </div>
   )
 }
