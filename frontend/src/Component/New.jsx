c4b2765f28d16ca6d9842f9098865b693edf6111
Yassin <mirza.yassin9@gmail.com>
2025-09-10 12:54:46 +0400

    frontend new complete

--- frontend/src/Component/New.jsx
+++ frontend/src/Component/New.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const New = ({category}) => {
   const [latest,setLatest] = useState({
@@ -9,11 +10,22 @@ const New = ({category}) => {
   useEffect(()=>{
     const fetchNew = async()=>{
       try {
-        
+      const res = await fetch("http://localhost:3100/api/new-collection"); 
+        const data = await res.json();
+        console.log("New Collection API:", data);
+
+        // this will trasform the backend array into object of keys
+        const transform = {
+          women: data.find((c)=> c.category === "women")?.images || [],
+          men : data.find((c)=>c.category === "men")?.images || [],
+          kid : data.find((c)=>c.category === "kid")?.images || [],
+        }
+        setLatest(transform)
       } catch (error) {
-        
+        console.error("Error fetching new collection:", error)
       }
     }
+    fetchNew()
   },[])
   return (
     <div>
@@ -23,7 +35,13 @@ const New = ({category}) => {
       </div>
       {/* All 12 images */}
       <div className='grid grid-cols-2 md:grid-cols-6'>
-        
+       {
+        latest[category]?.map((img,index)=>{
+          return <div className='w-full' key={index}>
+            <img src={`http://localhost:3100${img}`} alt="" className='w-full h-auto object-cover cursor-pointer'/>
+          </div>
+        })
+       }
       </div>
     </div>
   )
