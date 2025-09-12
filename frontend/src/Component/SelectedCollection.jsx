8168dc45e392cdc684d3f03e5b0936c7c4e73da7
Yassin <mirza.yassin9@gmail.com>
2025-09-12 11:35:02 +0400

    back and front complete

--- frontend/src/Component/SelectedCollection.jsx
+++ frontend/src/Component/SelectedCollection.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const SelectedCollection = () => {
+const SelectedCollection = ({ category }) => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchCollection = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3100/api/selected-collection/${category}`)
+        setItems(res.data?.items || [])
+      } catch (error) {
+        console.error(error)
+      }
+    };
+    fetchCollection()
+  }, [category])
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col px-4 md:py-3 lg:py-8 max-w-[1400px] mx-auto">
+      <h5 className="uppercase text-2xl font-medium md:mb-4 lg:mb-7">Selected collections</h5>
 
-export default SelectedCollection
+      {/* 4 images grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        {items.map((item, index) => (
+          <div key={index} className="">
+            <img src={`http://localhost:3100${item.image}`} className="w-full h-auto object-cover cursor-pointer" />
+            {/* images names */}
+            <p className="pt-3 text-2xl font-medium uppercase cursor-pointer">
+              {item.name}
+            </p>
+             {/* Explore */}
+      <h3 className="pt-3 text-2xl font-medium uppercase cursor-pointer">Explore</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SelectedCollection;
