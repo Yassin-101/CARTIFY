606d75685c3abb3a82fe07c444ec0a557ce35af1
Yassin <mirza.yassin9@gmail.com>
2025-10-05 18:36:44 +0400

    product frontend complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -72,5 +72,15 @@ const getProductById = async(req,res)=>{
       res.status(500).json({ message: "Failed to fetch product" });
   }
 }
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ products });
+  } catch (error) {
+    console.error("Error fetching all products:", error);
+    res.status(500).json({ message: "Failed to fetch all products" });
+  }
+};
 
-module.exports = {uploadProduct,getNewCollection,getSelectedCollection,getByCategory,getProductById}
+
+module.exports = {uploadProduct,getNewCollection,getSelectedCollection,getByCategory,getProductById,  getAllProducts}
